ee84931bef36547bb757b34854c858464bf46b1a
Jamina Godwin Carr <32621979+JaminaBGodwin@users.noreply.github.com>
2018-02-17 23:56:11 +0000

    Added a mock openGL context
    To allow testing of classes that require a running openGL context (i.e. that use any graphical assets.)

--- tests/src/de/tomgrill/gdxtesting/GdxTestRunner.java
+++ tests/src/de/tomgrill/gdxtesting/GdxTestRunner.java
@@ -24,6 +24,10 @@ import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import static org.mockito.Mockito.mock;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
@@ -37,6 +41,7 @@ public class GdxTestRunner extends BlockJUnit4ClassRunner implements Application
 		HeadlessApplicationConfiguration conf = new HeadlessApplicationConfiguration();
 
 		new HeadlessApplication(this, conf);
+		Gdx.gl = mock(GL20.class);
 	}
 
 	@Override
